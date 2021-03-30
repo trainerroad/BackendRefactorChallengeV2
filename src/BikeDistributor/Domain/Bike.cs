549272bdc3eb691aafb833aea469bb785daebafe
asalvo <git@stormex.net>
2021-03-30 16:56:47 -0500

    Initial Commit for Version 2

---
+++ src/BikeDistributor/Domain/Bike.cs
@@ -0,0 +1,20 @@
+﻿namespace BikeDistributor.Domain
+{
+    public class Bike
+    {
+        public const int OneThousand = 1000;
+        public const int TwoThousand = 2000;
+        public const int FiveThousand = 5000;
+
+        public Bike(string brand, string model, int price)
+        {
+            Brand = brand;
+            Model = model;
+            Price = price;
+        }
+
+        public string Brand { get; }
+        public string Model { get; }
+        public int Price { get; set; }
+    }
+}
---
+++ src/BikeDistributor/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace BikeDistributor.Infrastructure
+{
+    public interface IUnitOfWork
+    {
+        IQueryable<T> Query<T>();
+    }
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IQueryable<T> Query<T>()
+        {
+            //You do not need to implement this method for the coding exercise
+            throw new System.NotImplementedException();
+        }
+    }
+}
